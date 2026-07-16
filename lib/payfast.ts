134c67b8f51bdd54e660226b3bfd4c01ce468bbc
Juan-Lee <juanlee11@gmail.com>
2026-07-16 06:46:08 +0200

    Add PAYFAST_SANDBOX env var to force sandbox mode on production

--- lib/payfast.ts
+++ lib/payfast.ts
@@ -3,7 +3,7 @@ import crypto from 'crypto'
 export const PAYFAST_MERCHANT_ID = process.env.PAYFAST_MERCHANT_ID!
 export const PAYFAST_MERCHANT_KEY = process.env.PAYFAST_MERCHANT_KEY!
 export const PAYFAST_PASSPHRASE = process.env.PAYFAST_PASSPHRASE ?? ''
-export const IS_SANDBOX = process.env.NODE_ENV !== 'production'
+export const IS_SANDBOX = process.env.PAYFAST_SANDBOX === 'true' || process.env.NODE_ENV !== 'production'
 
 export const PAYFAST_URL = IS_SANDBOX
   ? 'https://sandbox.payfast.co.za/eng/process'

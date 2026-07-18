c4cfbd775965e72abb73f93112e853ff2b5813d9
Juan-Lee <juanlee11@gmail.com>
2026-07-18 07:17:38 +0200

    Increase logo size in navbar and footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar(props: NavbarProps) {
     <nav className="sticky top-0 z-50 bg-white border-b border-gray-100">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 h-14 sm:h-16 flex items-center justify-between">
         <Link href="/" className="flex items-center">
-          <Image src="/logo.png" alt="We Got Someone" width={160} height={40} priority className="h-8 sm:h-9 w-auto" />
+          <Image src="/logo.png" alt="We Got Someone" width={220} height={55} priority className="h-10 sm:h-12 w-auto" />
         </Link>
 
         {props.variant === 'public' && <NavMenu />}
